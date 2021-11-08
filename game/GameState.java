cebc8dc6b7737be90fa6487c15cdafa878671c32
Arcane-Panda <gigakong18@gmail.com>
2021-11-08 16:30:29 -0700

    Update GameState.java

--- game/GameState.java
+++ game/GameState.java
@@ -3,7 +3,18 @@
  */
 package game;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class GameState 
 {
-
+	//Fields
+	List<Animatable> gameObjects;
+	//list of animatable things
+	
+	public GameState ()
+	{
+		gameObjects = new ArrayList<Animatable>();
+	}
+	
 }
